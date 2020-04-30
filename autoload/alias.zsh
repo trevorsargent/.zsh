563b2abf6b17b33e57a9c865e8d9e844e6b4eaea
Trevor Sargent <trevor@lucid.rocks>
2020-04-30 16:57:43 -0700

    add sync alias

--- autoload/alias.zsh
+++ autoload/alias.zsh
@@ -2,5 +2,7 @@ alias zz="code ~/.zsh"
 alias zl=". ~/.zshrc"
 alias hh="code ~/.hyper.js"
 alias j="z"
+alias bb="git rev-parse --abbrev-ref HEAD"
+alias sync="git push -u origin $(bb)"
 
 alias weather='curl v2.wttr.in'  	
